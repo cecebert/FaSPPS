a8cf0c0421da513322b8b62be017329239449247
cecebert <cesarebertone@gmail.com>
2024-03-12 10:03:26 +0100

    Update

--- SPPS.py
+++ SPPS.py
@@ -28,8 +28,8 @@ resin_mass_empty=brutto_syr-tare_syr
 if tare_syr==brutto_syr:
     st.warning('Tare and brutto of the reactor cannot be identical, please verify your values')
 else:
-    
-    st.success('The net mass of the resin is: '+str(resin_mass_empty)+' g')
+    aminoacid_to_load=mw_aa*resin_mass_empty*2
+    st.success('The net mass of the resin is: '+str(np.round(resin_mass_empty, 4))+' g. You have to load ' + str(np.round(aminoacid_to_load, 4)) + ' mg of aminoacid')
     brutto_loaded_syr=st.number_input('Reactor Brutto after loading (g)', min_value=0.00001, value=3., format='%.5f')
 
     if brutto_loaded_syr==brutto_syr:
@@ -37,7 +37,7 @@ else:
         st.warning('Loaded mass and brutto of the reactor cannot be identical. If they are identical something has gone wrong, please verify your values')
     else:
         resin_mass_loaded=brutto_loaded_syr-tare_syr
-        st.success('The net mass of the aminoacid-loaded resin is: '+str(resin_mass_loaded)+' g')
+        st.success('The net mass of the aminoacid-loaded resin is: '+str(np.round(resin_mass_loaded, 4))+' g')
 
         loading_per_g, total_loading=gravimetric_load(brutto_loaded_syr, brutto_syr, tare_syr, mw_aa)
 
@@ -52,12 +52,12 @@ else:
 
             st.header('Next aminoacid')
             selected_aa2=st.selectbox('Choose the aminoacid to load', options=aa_df['Aminoacid'], key='2')
-            equivalents=st.number_input('Equivalents of aminoacid to load',min_value=0.01, value=1.5)
-
-            mass_aa=equivalents*total_loading*aa_df[aa_df['Aminoacid']==selected_aa2]['MW'].values[0]
-            mass_tbtu=equivalents*total_loading*mw_tbtu
-            mass_hobt=equivalents*total_loading*mw_hobt
-            mass_dipea=equivalents*3*total_loading*mw_dipea
+            equivalents_aa=st.number_input('Equivalents of aminoacid to load',min_value=0.01, value=1.6)
+            equivalents_activating_agent=st.number_input('Equivalents of activating agent to load',min_value=0.01, value=1.5)
+            mass_aa=equivalents_aa*total_loading*aa_df[aa_df['Aminoacid']==selected_aa2]['MW'].values[0]
+            mass_tbtu=equivalents_activating_agent*total_loading*mw_tbtu
+            mass_hobt=equivalents_activating_agent*total_loading*mw_hobt
+            mass_dipea=equivalents_activating_agent*3*total_loading*mw_dipea
             vol_dipea=mass_dipea/rho_dipea
 
             data_aa=np.array([mass_aa, mass_tbtu, mass_hobt, vol_dipea])
